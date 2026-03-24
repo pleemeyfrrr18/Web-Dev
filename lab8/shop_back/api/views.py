0db6a7fd7e3eb4289038b5d3ec1098ee7e961255
pleemeyfrrr18 <karamurzanovarsen@gmail.com>
2026-03-24 21:52:23 +0500

    Add lab8

---
+++ lab8/shop_back/api/views.py
@@ -0,0 +1,43 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Product, Category
+
+def product_list(request):
+    products = Product.objects.all()
+    data = list(products.values())
+    return JsonResponse(data, safe=False)
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    data = {
+        "id": product.id,
+        "name": product.name,
+        "price": product.price,
+        "description": product.description,
+        "count": product.count,
+        "is_active": product.is_active,
+        "category": product.category.id
+    }
+
+    return JsonResponse(data)
+
+def category_list(request):
+    categories = Category.objects.all()
+    data = list(categories.values())
+    return JsonResponse(data, safe=False)
+
+def category_detail(request, id):
+    category = get_object_or_404(Category, id=id)
+
+    data = {
+        "id": category.id,
+        "name": category.name
+    }
+
+    return JsonResponse(data)
+
+def category_products(request, id):
+    products = Product.objects.filter(category_id=id)
+    data = list(products.values())
+    return JsonResponse(data, safe=False)
